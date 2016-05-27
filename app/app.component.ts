56409acc566842d6728db5bf1bee729557c97726
Chris Harrell <chris.harrell@sq1.com>
2016-05-27 09:45:37 -0500

    Utilize the hero service to provide hero data to the app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
+    providers: [HeroService],
     directives: [HeroDetailComponent],
     template: `
         <h1>{{title}}</h1>
@@ -67,26 +69,22 @@ import { HeroDetailComponent } from './hero-detail.component';
     `]
 })
 
-export class AppComponent {
-    public heroes = HEROES;
+export class AppComponent implements OnInit {
+    heroes: Hero[];
     selectedHero: Hero;
-
     title = 'Tour of Heroes';
+
+    constructor(private heroService: HeroService) { }
+
+    ngOnInit() {
+        this.getHeroes();
+    }
+
+    getHeroes() {
+        this.heroes = this.heroService.getHeroes();
+    }
 
     onSelect(hero: Hero) {
         this.selectedHero = hero;
     }
 }
-
-var HEROES: Hero[] = [
-    { "id": 11, "name": "Mr. Nice" },
-    { "id": 12, "name": "Narco" },
-    { "id": 13, "name": "Bombasto" },
-    { "id": 14, "name": "Celeritas" },
-    { "id": 15, "name": "Magneta" },
-    { "id": 16, "name": "RubberMan" },
-    { "id": 17, "name": "Dynama" },
-    { "id": 18, "name": "Dr IQ" },
-    { "id": 19, "name": "Magma" },
-    { "id": 20, "name": "Tornado" }
-];
