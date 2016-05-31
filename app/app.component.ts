1851efbb829530951cfecb2a500306d5b63e0716
Chris Harrell <chris.harrell@sq1.com>
2016-05-31 08:57:22 -0500

    Added beginnings of application routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
-    providers: [HeroService],
-    directives: [HeroesComponent],
-    template: `<h1>{{title}}</h1>
-               <my-heroes></my-heroes>
+    providers: [HeroService, ROUTER_PROVIDERS],
+    directives: [ROUTER_DIRECTIVES],
+    template: `
+        <h1>{{title}}</h1>
+        <a [routerLink]="['Heroes']">Heroes</a>
+        <router-outlet></router-outlet>
     `,
     styles:[``]
 })
+
+@RouteConfig([
+    {
+        path: '/heroes',
+        name: 'Heroes',
+        component: HeroesComponent
+    }
+])
 
 export class AppComponent {
     title: 'Tour of Heroes'
