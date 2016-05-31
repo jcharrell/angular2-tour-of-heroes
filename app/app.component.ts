46ea71c006f8259fe163e501a5ee2e2220a100b0
Chris Harrell <chris.harrell@sq1.com>
2016-05-31 10:39:27 -0500

    Add route for hero details

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 
@@ -31,6 +32,11 @@ import { HeroService } from './hero.service';
         name: 'Dashboard',
         component: DashboardComponent,
         useAsDefault: true
+    },
+    {
+        path: '/detail/:id',
+        name: 'HeroDetail',
+        component: HeroDetailComponent
     }
 ])
 
