1f15c2a40e28b7a16aae63b9ec8d02312ea91819
Chris Harrell <chris.harrell@sq1.com>
2016-06-01 12:15:38 -0500

    Add CRUD methods to heroes component

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -7,36 +7,56 @@ import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-heroes',
-    providers: [],
     directives: [HeroDetailComponent],
     templateUrl: 'app/heroes.component.html',
     styleUrls: ['app/heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
     heroes: Hero[];
     selectedHero: Hero;
-    title = 'Tour of Heroes';
-
+    addingHero = false;
+    error: any;
+    
     constructor(
-        private heroService: HeroService,
-        private router: Router) {
-
-    }
-
+        private router: Router,
+        private heroService: HeroService) { }
+    
     ngOnInit() {
         this.getHeroes();
     }
-
+    
     getHeroes() {
-        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+        this.heroService
+            .getHeroes()
+            .then(heroes => this.heroes = heroes)
+            .catch(error => this.error = error); // TODO: Display error message
     }
-
-    gotoDetail() {
-        this.router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+    addHero() {
+        this.addingHero = true;
+        this.selectedHero = null;
+    }
+    close(savedHero: Hero) {
+        this.addingHero = false;
+        if (savedHero) { this.getHeroes(); }
+    }
+    delete(hero: Hero, event: any) {
+        event.stopPropagation();
+        this.heroService
+            .delete(hero)
+            .then(res => {
+                this.heroes = this.heroes.filter(h => h !== hero);
+                if (this.selectedHero === hero) { this.selectedHero = null; }
+            })
+            .catch(error => this.error = error); // TODO: Display error message
     }
 
     onSelect(hero: Hero) {
         this.selectedHero = hero;
+        this.addingHero = false;
     }
+    
+    gotoDetail() {
+        this.router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+    }
+
 }
